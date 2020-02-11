f9881a1818f8ef0d9dd8120c44abb1ff8356e7a9
Figueiredo Castro Fernandes <efigueiredo@indracompany.com>
2020-02-11 09:25:45 -0300

    Criando pasta com 2 exercicios feitos

---
+++ 4-dia/exercicios/Eric/script.js
@@ -0,0 +1,28 @@
+
+/* const x = document.getElementsByClassName("soma");
+var somatemp = 0;
+
+document.getElementById("btn-clicou").addEventListener("click", function(){
+
+  Array.from(x).forEach((elemento) => {
+      somatemp += parseInt(elemento.value);
+  })
+
+  alert(somatemp);
+}) */
+
+
+ /*  document.getElementById("btn-clicou").addEventListener("click", function(){
+    var x = document.getElementById("textodota").value;
+    
+    if (x.match("dota") == null) {
+
+      alert("Não tem Dota");
+    
+      } else {
+        alert("Tem dota");
+      }
+
+  }) */
+
+
