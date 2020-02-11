c9f9d4718348133db59a20bb9269a154358d59e1
tjeveraldo <everaldogomesbs@gmail.com>
2020-02-11 08:47:41 -0300

    Add Everaldo Exercicio

---
+++ 4-dia/exercicios/JEveraldo/cor.js
@@ -0,0 +1,11 @@
+document.querySelector("#verde").addEventListener("click", function(){
+    document.querySelector("#quadrado").style = "background:green;";
+  })
+  
+  document.querySelector("#vermelho").addEventListener("click", function(){
+    document.querySelector("#quadrado").style = "background:red;";
+  })
+
+  document.querySelector("#cinza").addEventListener("click", function(){
+    document.querySelector("#quadrado").style = "background:cinza;";
+  })
